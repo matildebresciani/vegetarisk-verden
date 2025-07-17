7a184182db95a7c81c7f9362de281f7b3aba8761
matilde <mabr0005@stud.kea.dk>
2025-07-17 18:42:10 +0200

    header border

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const Header = () => {
-    return ( <header className="flex justify-between items-center">
+    return ( <header className="flex justify-between items-center py-6 border-b-2 border-b-green-medium">
         <div className="flex flex-col">
             <span className="italic">SMAGEN AF EN</span>
             <span className="font-just-another-hand text-4xl">VEGETARISK VERDEN</span>
